223f69de4b570c304ef04293c5f2423d8bb5c761
thekingofbandit <thekingofbandit@gmail.com>
2019-09-18 18:32:13 +0700

    initial

---
+++ csc231/a3.php
@@ -0,0 +1,91 @@
+<?php
+require_once( "class.hangman.php" );
+print( "<h3>Hangman</h3>" );
+print( "Hangman  <b>Khanafi</b><br /><hr />" );
+print( "<link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css' integrity='sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T' crossorigin='anonymous'>" );
+session_start();
+
+if( isset( $_POST[ "new" ] ) )
+{
+	session_destroy();
+	print( "<script language='javascript'>window.location='a3.php'</script>" );
+}
+
+if( isset( $_SESSION[ "hangman" ] ) )
+{
+	$oGame = $_SESSION[ "hangman" ];
+} else {
+	$_SESSION[ "hangman" ] = new Hangman();
+	$oGame = $_SESSION[ "hangman" ];
+}
+
+if( isset( $_POST[ "submit" ] ) )
+{
+	if( strlen( $_POST[ "word" ] ) == 0 )
+	{
+		$oGame->checkLetter( $_POST[ "guess" ] );
+	} else {
+		$oGame->setGuessWord( $_POST[ "word" ] );
+	}
+}
+
+print( "Current word: " );
+$oGame->printWord();
+print( "<br /><hr />" );
+
+if( $oGame->iStatus != 0 )
+{
+	print( "Current status: <img src='" . $oGame->getStatus() . "' />" );
+	print( "<br /><hr />" );
+}
+
+if( sizeof( $oGame->aTriedLetters ) != 0 )
+{
+	print( "Tried letters: " );
+	for( $i = 0; $i < sizeof( $oGame->aTriedLetters ); $i++ )
+	{
+		print( $oGame->aTriedLetters[ current( $oGame->aTriedLetters ) ] . ", " );
+		next( $oGame->aTriedLetters );
+	}
+	print( "<br /><hr />" );
+}
+
+if( $oGame->checkDead() )
+{
+	session_destroy();
+	print( "<h3>You lost!</h3>" );
+	print( "The word was '" . $oGame->sWord . "'<br />" );
+	print( "<a href='a3.php'>Play again</a>" );
+	exit();
+}
+
+if( $oGame->wordGuessed() )
+{
+	session_destroy();
+	print( "<h3>You won!</h3>" );
+	print( "You guessed the word '" . $oGame->sWord . "' " );
+	print( "in <b>" . $oGame->iGuesses . "</b> guesses<br />" );
+	print( "<a href='a3.php' class='btn btn-primary'>Play again</a>" );
+	exit();
+}
+
+$_SESSION[ "hangman" ] = $oGame;
+
+print( "<form method='POST' action='a3.php'>" );
+print( "Guess letter: " );
+print( "<select name='guess' class='form-control col-1'>" );
+for( $i = 97; $i < 123; $i++ )
+{
+	if( array_key_exists( chr( $i ), $oGame->aTriedLetters ) )
+	{
+		print( "<option disabled>" . chr( $i ) ."</option>" );
+	} else {
+		print( "<option>" . chr( $i ) . "</option>" );
+	}
+}
+print( "</select>" );
+print( "<br />" );
+print( "Guess word: <input type='text' name='word' class='input-group-text'/><br />" );
+print( "<input type='submit' name='submit' value='Guess'  class='btn btn-primary'/>&nbsp;" );
+print( "<input type='submit' name='new' value='New game'  class='btn btn-primary'/>" );
+print( "</form>" );
---
+++ csc231/class.hangman.php
@@ -0,0 +1,159 @@
+<?php
+
+ob_start();
+
+class Hangman
+{
+	//The word the user has to guess in order to win the game
+	var $sWord;
+	
+	//The word the user guessed in the 'guess word' field
+	var $sWordGuess;
+	
+	//The length of the hangman word
+	var $iWordLength;
+	
+	//letters that were already tried
+	var $aTriedLetters = array();
+	
+	//array of letters the word contains
+	var $aLetters = array();
+	
+	//array of status-images
+	var $aImages = array();
+	
+	//array of keys which are guessed
+	var $aGuessedKeys = array();
+	
+	//game status
+	var $iStatus;
+	
+	//the number of guesses the user made
+	var $iGuesses;
+	
+	//initialize the game variables
+	
+	function Hangman()
+	{
+		//chose a random word from the $words array
+		$words = array( "apple", "banana", "orange", "papaya", "mango" );
+		//$words = fetchWordArray($WORDLISTFILE);
+		$this->sWord = $words[ rand( 0, sizeof( $words ) ) ];
+		
+		//define the word's length
+		$this->iWordLength = strlen( $this->sWord );
+		
+		//fill the array with the individual letters of the word
+		for( $i = 0; $i < $this->iWordLength; $i++ )
+		{
+			$this->aLetters[ $i ] = substr( $this->sWord, $i, 1 );
+		}
+		
+		//fill the array with the status images, located in the 'images' folder
+		for( $i = 1; $i <= 11; $i++ )
+		{
+			$this->aImages[ $i ] = "images/$i.jpeg";
+		}
+		
+		//other variables
+		$this->iStatus = 0;
+		$this->iGuesses = 0;
+	}
+	
+	//check if the letter '$sLetter' is in the word, if so, add it to the guessed keys
+	function checkLetter( $sLetter )
+	{
+		$aTemp = array_values( $this->aLetters );
+		$iCount = 0;
+		//for each value in the '$aTemp' array, check if the value equals '$sLetter'
+		for( $i = 0; $i < sizeof( $aTemp ); $i++ )
+		{
+			if( $aTemp[ $i ] == $sLetter )
+			{
+				//if it is, add the letter to the guessed keys and add one to '$iCount'
+				$this->aGuessedKeys[ $i ] = $i;
+				$iCount++;
+			}
+		}
+		
+		//if the letter wasn't in the word, update the hangman status
+		if( $iCount == 0 )
+		{
+			$this->iStatus++;
+		}
+		
+		//add the letter to the tried letters and update the amount of guesses
+		$this->aTriedLetters[ $sLetter ] = $sLetter;
+		$this->iGuesses++;
+		
+		return;
+	}
+	
+	//if the user guessed a word, set the class's property to '$sWord'
+	function setGuessWord( $sWord )
+	{
+		$this->sWordGuess = $sWord;
+		
+		//check if '$sWord' is the same as the word that needs to be guessed
+		if( $this->sWordGuess != $this->sWord )
+		{
+			//if it isn't, update the status
+			$this->iStatus++;
+		}
+		
+		//update the guesses
+		$this->iGuesses++;
+		
+		return true;
+	}
+	
+	//check if the word is guessed already
+	function wordGuessed()
+	{
+		if( sizeof( $this->aGuessedKeys ) == $this->iWordLength 
+		    || $this->sWordGuess == $this->sWord )
+		{
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	//check if the user is dead
+	function checkDead()
+	{
+		if( $this->iStatus == 11 )
+		{
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	//return the relative URL of the status image
+	function getStatus()
+	{
+		return $this->aImages[ $this->iStatus ];
+	}
+	
+	//print the word
+	function printWord()
+	{
+		for( $i = 0; $i < $this->iWordLength; $i++ )
+		{
+			//if the letter was guessed
+			if( array_key_exists( $i, $this->aGuessedKeys ) )
+			{
+				//print the letter
+				print( substr( $this->sWord, $i, 1 ) . "&nbsp;" );
+			} else {
+				//print a underscore( _ )
+				print( "_&nbsp;" );
+			}
+		}
+		
+		return;
+	}
+}
+
+?>
